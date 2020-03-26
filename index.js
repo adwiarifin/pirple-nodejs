1f9ae2aebfab7e0c354cd1005eeda68f1f6ffd06
Adwi Arifin <adwi@itmi.id>
2020-03-26 16:27:10 +0700

    move index to lib (server & worker)

--- index.js
+++ index.js
@@ -1,21 +1,13 @@
-const http = require('http');
-const https = require('https');
-const config =  require('./lib/config');
-const server = require('./server');
-const fs = require('fs');
+const server = require('./lib/server');
+const worker = require('./lib/worker');
 
-// create server
-const httpsOptions = {
-    key: fs.readFileSync('./https/key.pem'),
-    cert: fs.readFileSync('./https/cert.pem')
+const app = {
+    init: function() {
+        server.init();
+        worker.init();
+    }
 }
-const httpsServer = https.createServer(httpsOptions, server);
-const httpServer = http.createServer(server);
 
-// server listen
-httpServer.listen(config.httpPort, function () {
-    console.log(`The server is listening on port ${config.httpPort} in ${config.envName} now`);
-});
-httpsServer.listen(config.httpsPort, function () {
-    console.log(`The server is listening on port ${config.httpsPort} in ${config.envName} now`);
-});
+app.init();
+
+module.exports = app;
